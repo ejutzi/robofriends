ddc8b22fab5d01b12f5bb6f3f87b04ebc5cfe6fd
ejutzi <65071377+ejutzi@users.noreply.github.com>
2020-06-03 18:59:31 +0200

    Add files via upload
    initial project commit

---
+++ src/robots.js
@@ -0,0 +1,26 @@
+export const robots = [
+	{
+		id: 1,
+		name: 'Bernd Beispiel',
+		username: 'BBernd',
+		email: 'bbernd@gmail.com'
+	},
+	{
+		id: 2,
+		name: 'Viktor Vorschau',
+		username: 'VViktor',
+		email: 'vviktor@gmail.com'
+	},
+	{
+		id: 3,
+		name: 'Max Muster',
+		username: 'MMax',
+		email: 'mmax@gmail.com'
+	},
+	{
+		id: 4,
+		name: 'Timo Test',
+		username: 'TTimo',
+		email: 'ttimo@gmail.com'
+	}
+]
