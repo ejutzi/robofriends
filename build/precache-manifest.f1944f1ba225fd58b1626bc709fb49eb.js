f766daa604f33bc2e1c803f1db326bb40c726236
Naikx <erik.jutzi@googlemail.com>
2020-06-04 14:55:44 +0200

    code revisited

---
+++ build/precache-manifest.f1944f1ba225fd58b1626bc709fb49eb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee1e38bf730518e708939d2218c1c43b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4add6107f8fd2eb1c82",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "af8c836618403f02e520",
+    "url": "/static/css/main.5b39b65f.chunk.css"
+  },
+  {
+    "revision": "d4add6107f8fd2eb1c82",
+    "url": "/static/js/2.7ec5fd90.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7ec5fd90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af8c836618403f02e520",
+    "url": "/static/js/main.ba12ec60.chunk.js"
+  },
+  {
+    "revision": "fdb66bf0534857f19626",
+    "url": "/static/js/runtime-main.8f3ae637.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
